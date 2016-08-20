9d9f22f424f78c64d3d661969e9692f21976f992
joshib <joshianandbe1995@gmail.com>
2016-08-20 17:54:58 +0530

    Add files via upload

---
+++ ass3 1.java
@@ -0,0 +1,30 @@
+
+package revnumb;
+
+import java.util.Scanner;
+
+
+public class rn{
+    
+    public static void main(String[] args) {
+       
+        {
+int[]a;
+int n;
+Scanner input=new Scanner(System.in);
+System.out.print("enter the array:");
+n=input.nextInt();
+a=new int[n];
+System.out.println("enter"+n+"value:");
+for(int i=0;i< a.length;i++)
+a[i]=input.nextInt();
+for(int i=0,j=n-1;i<n/2;i++,j--)
+a[i]=a[i]+a[j]-(a[j]=a[i]);
+System.out.println("Reverse order");
+for(int i=0;i<a.length;i++)
+System.out.println(a[i]);
+}
+} 
+}
+    
+
