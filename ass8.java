ebeb0ff9af63ece47782e6429a74655acb22c518
joshib <joshianandbe1995@gmail.com>
2016-08-22 18:39:48 +0530

    Add files via upload

---
+++ ass8.java
@@ -0,0 +1,18 @@
+
+
+
+import java.util.Scanner;
+
+
+public class Kavi {
+
+    
+    public static void main(String[] args) {
+      int j;
+      Scanner in= new Scanner(System.in);
+      j=in.nextInt();
+      for(j=0;j<5;j++);
+      System.out.println("Hello");
+    }
+    
+}
