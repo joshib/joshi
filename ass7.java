499674af80f8a3b7a519df4f25dc064075c3d854
joshib <joshianandbe1995@gmail.com>
2016-08-22 22:16:18 +0930

    Add files via upload

---
+++ ass7.java
@@ -0,0 +1,21 @@
+
+
+
+import java.util.Scanner;
+
+
+public class Goki {
+
+       public static void main(String[] args) {
+    int x;
+    Scanner in= new Scanner(System.in);
+    x=in.nextInt();
+    if(x%2==0)
+    {
+        System.out.println("Even number");
+    }
+    else
+        System.out.println("Odd number");
+    }
+    }
+
