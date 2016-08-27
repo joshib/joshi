83a12c3973840e8348c1a185e92f5efc722d5c6e
joshib <joshianandbe1995@gmail.com>
2016-08-27 16:58:56 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,17 @@
+ public static void main(String[] args) {
+        int b;
+        Scanner in =new Scanner (System.in);
+        System.out.println("Enter the year:");
+        b=in.nextInt();
+        if(b%4==0)
+        {
+            System.out.println("The given year is leap year");
+        }
+        else
+            System.out.println("The given year is not a leap year");
+        
+        
+        
+    }
+    
+
