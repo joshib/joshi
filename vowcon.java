2c089a728d124fe91010cbc87f672f9b56b7ba4c
joshib <joshianandbe1995@gmail.com>
2016-08-27 17:37:01 +0530

    Add files via upload

---
+++ vowcon.java
@@ -0,0 +1,15 @@
+{
+        char ch;
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter the character");
+        ch=(char)System.in.read();
+        if (ch== 'a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        {
+        System.out.println("The given character is a vowel");
+        }
+        else
+        System.out.println("The given character is a consonant");
+    
+        }       
+        
+        
